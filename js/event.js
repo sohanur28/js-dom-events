fb3b436481030e9c4fe9e9793bbc9dda3f65d917
sohanur28 <sohanur28@gmail.com>
2025-06-16 11:44:31 +0600

    onclick handler

--- js/event.js
+++ js/event.js
@@ -1,1 +1,21 @@
-console.log('This is separate JS file');
+// option 1: directly set on the HTML element
+
+
+// optiion 2: add onclick function
+function makeRed(){
+            document.body.style.backgroundColor='red';
+        }
+
+// option: 3
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
+
+// option: 3 another
+const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'purple';
+        }        
